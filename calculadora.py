8dba08a15d9479e559a0d9cc16df68e870a029fc
yramos93 <101157490+yramos93@users.noreply.github.com>
2023-01-15 00:24:54 +0000

    segundocommit

--- calculadora.py
+++ calculadora.py
@@ -18,7 +18,6 @@ def raiz(x):
 def exponente(x,y):
     return x ** y
 
-
 print("Seleccionar la operación a realizar.")
 print("1.Suma")
 print("2.Resta")
@@ -36,41 +35,41 @@ while True:
     if choice in ('1', '2', '3', '4', '5', '6', '7','8'):
         
         if choice == '1':
-            num1 = float(input("Captura 1 numero: "))
-            num2 = float(input("Captura 2 numero: "))
+            num1 = float(input("Captura el primer numero: "))
+            num2 = float(input("Captura el segundo numero: "))
             print(num1, "+", num2, "=", add(num1, num2))
 
         elif choice == '2':
-            num1 = float(input("Captura 1 numero: "))
-            num2 = float(input("Captura 2 numero: "))
+            num1 = float(input("Captura el primer numero: "))
+            num2 = float(input("Captura el segundo numero: "))
             print(num1, "-", num2, "=", subtract(num1, num2))
 
         elif choice == '3':
-            num1 = float(input("Captura 1 numero: "))
-            num2 = float(input("Captura 2 numero: "))
+            num1 = float(input("Captura el primer numero: "))
+            num2 = float(input("Captura el segundo numero: "))
             print(num1, "*", num2, "=", multiply(num1, num2))
 
         elif choice == '4':
-            num1 = float(input("Captura 1 numero: "))
-            num2 = float(input("Captura 2 numero: "))
+            num1 = float(input("Captura el primer numero: "))
+            num2 = float(input("Captura el segundo numero: "))
             print(num1, "/", num2, "=", divide(num1, num2))
         
         elif choice == '5':
-            num1 = float(input("Captura 1 numero: "))
+            num1 = float(input("Captura el primer numero: "))
             print(num1, "^" "=", raiz(num1))
         
         elif choice == '6':
-            num1 = float(input("Captura 1 numero: "))
+            num1 = float(input("Captura el primer numero: "))
             print(num1, "**",num2, "=", exponente(num1,num2))
         
         elif choice == '7':
-            num1 = float(input("Captura 1 numero: "))
+            num1 = float(input("Captura el primer numero: "))
             s1 = float (num1)
             s2 = math.radians(s1)
             print(num1, "Seno", "=",math.sin(s2))
         
         elif choice == '8':
-            num1 = float(input("Captura 1 numero: "))
+            num1 = float(input("Captura el primer numero: "))
             s1 = float (num1)
             s2 = math.radians(s1)
             print(num1, "Coseno", "=",math.cos(s2))
@@ -79,6 +78,7 @@ while True:
         next_calculation = input("Desea continuar con otra operación? (si/no): ")
         if next_calculation == "no":
           break
-    #Validación de numero no permitidos
+
+    #Solo valores del 1-8
     else:
         print("Numero invalido")
